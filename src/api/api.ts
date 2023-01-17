cb7f94ce951a3ec03f7adf9d0fc9b69ce1a6f4aa
jiyoonZ <wldbsl416@gmail.com>
2023-01-18 02:59:41 +0900

    Feat : 추가/삭제 slice 생성

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,14 @@
+import { IComment } from '../type';
 import { axiosClient } from './axiosClient';
 
 export const getComments = () => {
   return axiosClient.get('/');
+};
+
+export const postComment = (comment: IComment) => {
+  return axiosClient.post('/', comment);
+};
+
+export const delComment = (commentId: number) => {
+  return axiosClient.delete(`/${commentId}`);
 };
--- src/slice/commentSlice.ts
+++ src/slice/commentSlice.ts
@@ -1,26 +1,27 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { AxiosResponse } from 'axios';
-import { getComments } from '../api/api';
+import { delComment, getComments, postComment } from '../api/api';
+import { CommentsState, IComment } from '../type';
 
-export interface IComments {
-  id: number;
-  profile_url: string;
-  author: string;
-  content: string;
-  createdAt: string;
-}
-export interface CommentsState {
-  value: IComments[];
-}
 const INIT_STATE: CommentsState = {
   value: [],
 };
 
-export const fetchComments = createAsyncThunk(
-  'comments/fetchComments',
-  async () => {
-    const commentsList = await getComments();
-    return commentsList;
+export const fetchComments = createAsyncThunk('comments/fetchComments', () => {
+  const commentsList = getComments();
+  return commentsList;
+});
+
+export const addComment = createAsyncThunk(
+  'comments/addComment',
+  (commentData: IComment) => {
+    return postComment(commentData);
+  },
+);
+export const deleteComment = createAsyncThunk(
+  'comment/deleteComment',
+  (commentId: number) => {
+    delComment(commentId);
+    return commentId;
   },
 );
 
@@ -29,9 +30,16 @@ export const commentSlice = createSlice({
   initialState: INIT_STATE,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchComments.fulfilled, (state, { payload }: any) => {
-      state.value = [...state.value, ...payload];
-    });
+    builder
+      .addCase(fetchComments.fulfilled, (state, { payload }: any) => {
+        state.value = [...payload];
+      })
+      .addCase(addComment.fulfilled, (state, { payload }: any) => {
+        state.value = [...state.value, payload];
+      })
+      .addCase(deleteComment.fulfilled, (state, { payload }: any) => {
+        state.value = state.value.filter((ele) => ele.id !== payload);
+      });
   },
 });
 
