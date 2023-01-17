a552a798a1fcd2075902f9dc72aea2025804fae6
jiyoonZ <wldbsl416@gmail.com>
2023-01-18 03:00:40 +0900

    Fix : 타입 수정

---
+++ src/type/index.ts
@@ -0,0 +1,10 @@
+export interface IComment {
+  id?: number;
+  profile_url: string;
+  author: string;
+  content: string;
+  createdAt: string;
+}
+export interface CommentsState {
+  value: IComment[];
+}
