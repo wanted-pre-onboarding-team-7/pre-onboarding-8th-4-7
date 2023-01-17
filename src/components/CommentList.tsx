b286243ebb6f88508a7e638a6240e06900ff37f4
sujin <soojin9877@naver.com>
2023-01-17 13:44:04 +0900

    Feat: get, post 요청 구현

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { axiosInstance } from '../util/axiosInstance';
 import styled from 'styled-components';
 
 const Comment = styled.div`
@@ -26,7 +27,7 @@ const Content = styled.div`
 const Button = styled.div`
   text-align: right;
   margin: 10px 0;
-  & > a {
+  & > button {
     margin-right: 10px;
     padding: 0.375rem 0.75rem;
     border-radius: 0.25rem;
@@ -36,37 +37,56 @@ const Button = styled.div`
 `;
 
 // 임시 데이터 입니다. 코드 작성시 data 부분을 지워주세요
-const data = [
-  {
-    id: 1,
-    profile_url: 'https://picsum.photos/id/1/50/50',
-    author: 'abc_1',
-    content: 'UI 테스트는 어떻게 진행하나요',
-    createdAt: '2020-05-01',
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     profile_url: 'https://picsum.photos/id/1/50/50',
+//     author: 'abc_1',
+//     content: 'UI 테스트는 어떻게 진행하나요',
+//     createdAt: '2020-05-01',
+//   },
+// ];
+
+interface Icomment {
+  id: number;
+  profile_url: string;
+  author: string;
+  content: string;
+  createdAt: string;
+}
 
 function CommentList() {
+  const [comments, setComments] = useState([]);
+  const getComments = async () => {
+    try {
+      const response = await axiosInstance.get('');
+      console.log(response.data);
+      setComments(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getComments();
+  }, []);
+
   return (
     <>
-      {data.map((comment, key) => (
-        <Comment key={key}>
-          <img src={comment.profile_url} alt="" />
-
-          {comment.author}
-
-          <CreatedAt>{comment.createdAt}</CreatedAt>
-
-          <Content>{comment.content}</Content>
-
-          <Button>
-            <a>수정</a>
-            <a>삭제</a>
-          </Button>
-
-          <hr />
-        </Comment>
-      ))}
+      {comments.map(
+        ({ id, profile_url, author, content, createdAt }: Icomment) => (
+          <Comment key={id}>
+            <img src={profile_url} alt="" />
+            {author}
+            <CreatedAt>{createdAt}</CreatedAt>
+            <Content>{content}</Content>
+            <Button>
+              <button type="button">수정</button>
+              <button type="button">삭제</button>
+            </Button>
+            <hr />
+          </Comment>
+        ),
+      )}
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useRef } from 'react';
 import styled from 'styled-components';
+import { axiosInstance } from '../util/axiosInstance';
 
 const FormStyle = styled.div`
   & > form {
@@ -25,9 +26,27 @@ const FormStyle = styled.div`
 `;
 
 function Form() {
+  const authorRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+  const dateRef = useRef<HTMLInputElement>(null);
+
+  const postComment = async () => {
+    try {
+      const response = await axiosInstance.post('', {
+        profile_url: 'https://picsum.photos/id/27/50/50',
+        author: authorRef.current?.value,
+        content: contentRef.current?.value,
+        createdAt: dateRef.current?.value,
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <FormStyle>
-      <form>
+      <form onSubmit={postComment}>
         <input
           type="text"
           name="profile_url"
@@ -35,11 +54,22 @@ function Form() {
           required
         />
         <br />
-        <input type="text" name="author" placeholder="작성자" />
+        <input type="text" name="author" placeholder="작성자" ref={authorRef} />
         <br />
-        <textarea name="content" placeholder="내용" required></textarea>
+        <textarea
+          name="content"
+          placeholder="내용"
+          ref={contentRef}
+          required
+        ></textarea>
         <br />
-        <input type="text" name="createdAt" placeholder="2020-05-30" required />
+        <input
+          type="text"
+          name="createdAt"
+          placeholder="2020-05-30"
+          ref={dateRef}
+          required
+        />
         <br />
         <button type="submit">등록</button>
       </form>
